d49f3c9548927ce95714c4371753acd2d48d02f0
Eric Herman <eric@freesa.org>
2020-03-07 15:02:43 +0000

    basic testing
    co-authored-by: Ace Medlock <ace.medlock@gmail.com>
    co-authored-by: Kendrick Shaw <kms15@case.edu>
    co-authored-by: Rutger Bazen <rutger.bazen@gmail.com>

--- fake-sample-server/foo.js
+++ fake-sample-server/foo.js
@@ -1,20 +1,96 @@
-
+"use strict";
 var fs = require('fs'),
-    http = require('http');
+    http = require('http'),
+    assert = require('assert');
 
-const hostname = 'localhost';
+const hostname = '127.0.0.1';
 const port = 8080;
+const testMaxMillis = (1 * 1000);
 
-http.createServer(function (req, res) {
-  fs.readFile(__dirname + "/static/" + req.url, function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(data);
-  });
+http.createServer(function(req, res) {
+    fs.readFile(__dirname + "/static/" + req.url, function(err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end(data);
+    });
 }).listen(port, hostname, () => {
-  console.log(`Node.js server is running on http://${hostname}:${port}/`);
+    console.log(`Node.js server is running on http://${hostname}:${port}/`);
 });
+
+http.createServer(function(req, res) {
+    res.writeHead(500);
+    res.end();
+    console.log(`In second server`);
+}).listen('8081', hostname);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function testBaz(nextTest) {
+    console.log("testBaz");
+    nextTest();
+}
+
+function myFunc(callback) {
+    console.log(`Halfway dead`);
+    callback();
+}
+
+function testBar(callback) {
+    console.log("testBar");
+    setInterval(
+        (function() {
+            myFunc(callback)
+        }),
+        testMaxMillis / 2);
+}
+
+function testFoo(callback) {
+    console.log("testFoo");
+    assert(1 == 1);
+    callback();
+}
+
+function runTests(tests) {
+    assert(Array.isArray(tests));
+    var numTestsExpected = tests.length;
+    var numTestsCalled = 0;
+    var testStartMillis = Date.now();
+    var theTest = function() {
+        console.log(`Ran ${numTestsCalled}/${numTestsExpected} tests`)
+        assert(numTestsCalled == numTestsExpected);
+        var testEndMillis = Date.now();
+        var testTimeMillis = testEndMillis - testStartMillis;
+        console.log(`time: ${testTimeMillis}`);
+        console.log('*** tests pass ***');
+        process.exit(0);
+    };
+    while (tests.length) {
+        let currentTest = tests.pop();
+        let nextTest = theTest;
+        theTest = function() {
+            ++numTestsCalled;
+            currentTest(nextTest);
+        }
+    }
+    theTest()
+}
+
+
+runTests(
+    [
+        testFoo, testBaz, testBar
+    ]
+)
+
+setInterval((function() {
+    console.log('####### Timeout exceeded ########');
+    process.exit(1);
+}), testMaxMillis);
