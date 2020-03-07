f2390ab474cf3d51220a2bac8b5c2a8790a5c841
Ace Medlock <ace.medlock@gmail.com>
2020-03-07 17:24:44 +0000

    Simple websocket test
    co-authored-by: Rutger Bazen <rutger.bazen@gmail.com>
    co-authored-by: Eric Herman <eric@freesa.org>
    co-authored-by: Kendrick Shaw <kms15@case.edu>

--- fake-sample-server/foo.js
+++ fake-sample-server/foo.js
@@ -1,13 +1,16 @@
 "use strict";
+
 var fs = require('fs'),
     http = require('http'),
     assert = require('assert');
+
+const WebSocket = require('ws');
 
 const hostname = '127.0.0.1';
 const port = 8080;
 const testMaxMillis = (1 * 1000);
 
-http.createServer(function(req, res) {
+const server = http.createServer(function(req, res) {
     fs.readFile(__dirname + "/static/" + req.url, function(err, data) {
         if (err) {
             res.writeHead(404);
@@ -19,9 +22,42 @@ http.createServer(function(req, res) {
         });
         res.end(data);
     });
-}).listen(port, hostname, () => {
+});
+
+const wss1 = new WebSocket.Server({
+    noServer: true
+});
+
+server.on('upgrade', function upgrade(request, socket, head) {
+    const pathname = request.url;
+    if (pathname === '/foo') {
+        wss1.handleUpgrade(request, socket, head, function done(ws) {
+            ws.send('Hello world')
+        });
+        /*  } else if (pathname === '/bar') {
+            wss2.handleUpgrade(request, socket, head, function done(ws) {
+              wss2.emit('connection', ws, request);
+            });
+        */
+    } else {
+        socket.destroy();
+    }
+});
+
+server.listen(port, hostname, () => {
     console.log(`Node.js server is running on http://${hostname}:${port}/`);
 });
+
+function testSocketReceive(callback) {
+    const ws = new WebSocket(`ws://${hostname}:${port}/foo`);
+    ws.on('message', function incoming(data) {
+        console.log("In: testSocketReceive");
+        console.log(data);
+        callback();
+    });
+
+}
+
 
 function testIndexPage(callback) {
     const options = {
@@ -70,6 +106,7 @@ function runTests(tests) {
 runTests(
     [
         testIndexPage,
+        testSocketReceive,
     ]
 )
 
