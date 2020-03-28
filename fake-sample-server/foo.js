3585437801eb0748154786739064e1f60939655b
Ace Medlock <ace.medlock@gmail.com>
2020-03-28 16:32:09 +0000

    Add test of parallel receivers
    Also factor out connect message string,
    shorten timeout to 1/2 second.
    
    co-authored-by: Eric Herman <eric@freesa.org>
    co-authored-by: Kendrick Shaw <kms15@case.edu>

--- fake-sample-server/foo.js
+++ fake-sample-server/foo.js
@@ -7,7 +7,8 @@ var fs = require('fs'),
 
 const hostname = '127.0.0.1';
 const port = 8080;
-const testMaxMillis = (1 * 1000);
+const testMaxMillis = (0.5 * 1000); // half second
+const echoServerConnectMessage = 'Hello world';
 
 const server = http.createServer(function(req, res) {
     fs.readFile(__dirname + "/static/" + req.url, function(err, data) {
@@ -33,7 +34,7 @@ server.on('upgrade', function upgrade(request, socket, head) {
     const pathname = request.url;
     if (pathname === '/sub') {
         wss1.handleUpgrade(request, socket, head, function done(ws) {
-            ws.send('Hello world');
+            ws.send(echoServerConnectMessage);
             receivers.add(ws);
             ws.on('close', function clear() {
                 receivers.delete(ws);
@@ -56,6 +57,13 @@ server.listen(port, hostname, () => {
     console.log(`Node.js server is running on http://${hostname}:${port}/`);
 });
 
+function assertEquals(expected, actual) {
+    if (expected !== actual) {
+        console.error(`expected "${expected}" but was "${actual}"`);
+        assert(false);
+    }
+}
+
 function assertContains(needle, haystack) {
     if (haystack.indexOf(needle) == -1) {
         console.error(`expected "${needle}" in "${haystack}"`);
@@ -67,7 +75,7 @@ function testSocketReceive(callback) {
     const ws = new WebSocket(`ws://${hostname}:${port}/sub`);
     ws.on('message', function incoming(data) {
         var pageString = data.toString();
-        assert(pageString.indexOf("Hello world") !== -1);
+        assertContains(echoServerConnectMessage, pageString);
         ws.terminate();
         callback();
     });
@@ -78,7 +86,7 @@ function testEcho(callback) {
     const wspub = new WebSocket(`ws://${hostname}:${port}/pub`);
 
     var expected = [
-        "Hello world",
+        echoServerConnectMessage,
         "one",
         "two",
     ];
@@ -93,8 +101,81 @@ function testEcho(callback) {
         if (cnt < expected.length) {
             wspub.send(expected[cnt]);
         } else {
+            wssub.terminate();
+            wspub.terminate();
             callback();
         }
+    });
+}
+
+
+function testParallelReceive(callback) {
+    var wssubs = [];
+    const wspub = new WebSocket(`ws://${hostname}:${port}/pub`);
+
+    var expected = [
+        "one",
+        "two",
+        "three",
+    ];
+    var messagesBySocket = [];
+    var sendCount = 0;
+    var receiveCount = 0;
+
+    function assertAllMessagesExpected() {
+        assertEquals(messagesBySocket.length, expected.length);
+        // assertions go here
+        var i, j;
+        for (i = 0; i < messagesBySocket.length; ++i) {
+            var numMessagesExpected = expected.length + 1 - i;
+            assertEquals(messagesBySocket[i].length, numMessagesExpected);
+            assertEquals(messagesBySocket[i][0], echoServerConnectMessage);
+            for (j = 1; j < numMessagesExpected; ++j) {
+                var expMsg = expected[j + i - 1];
+                assertEquals(messagesBySocket[i][j], expMsg);
+            }
+        }
+    };
+
+    function createSocket() {
+        var newws = new WebSocket(`ws://${hostname}:${port}/sub`);
+        wssubs.push(newws);
+        var messages = [];
+        newws.on('message', recorderCallback(messages));
+        messagesBySocket.push(messages);
+    }
+
+    function recorderCallback(messageList) {
+        return (data) => {
+            var pageString = data.toString();
+            messageList.push(pageString);
+
+            if (pageString == echoServerConnectMessage) {
+                return;
+            }
+
+            receiveCount++;
+            if (receiveCount >= wssubs.length) {
+                sendCount++;
+                receiveCount = 0;
+                if (sendCount < expected.length) {
+                    createSocket();
+                    wspub.send(expected[sendCount]);
+                } else {
+                    assertAllMessagesExpected();
+                    wssubs.forEach((ws) => {
+                        ws.terminate();
+                    });
+                    wspub.terminate();
+                    callback();
+                }
+            }
+        };
+    };
+
+    createSocket();
+    wspub.on('open', () => {
+        wspub.send(expected[0]);
     });
 }
 
@@ -109,8 +190,7 @@ function testIndexPage(callback) {
     const req = http.request(options, res => {
         res.on('data', data => {
             var pageString = data.toString();
-            assert(pageString.indexOf("Test page") !== -1);
-            assert(pageString.match(/Test page/));
+            assertContains("Test page", pageString);
             callback();
         })
     })
@@ -124,7 +204,7 @@ function runTests(tests) {
     var testStartMillis = Date.now();
     var theTest = function() {
         console.log(`Ran ${numTestsCalled}/${numTestsExpected} tests`)
-        assert(numTestsCalled == numTestsExpected);
+        assertEquals(numTestsExpected, numTestsCalled);
         var testEndMillis = Date.now();
         var testTimeMillis = testEndMillis - testStartMillis;
         console.log(`time: ${testTimeMillis}`);
@@ -136,11 +216,14 @@ function runTests(tests) {
         let currentTest = tests.pop();
         let nextTest = theTest;
         theTest = function() {
+            console.log('* ' + currentTest.name);
             ++numTestsCalled;
-            currentTest(nextTest);
+            currentTest((test) => {
+                nextTest(test);
+            });
         }
     }
-    theTest()
+    theTest();
 }
 
 runTests(
@@ -148,8 +231,9 @@ runTests(
         testIndexPage,
         testSocketReceive,
         testEcho,
+        testParallelReceive,
     ]
-)
+);
 
 setInterval((function() {
     console.log('####### Timeout exceeded ########');
