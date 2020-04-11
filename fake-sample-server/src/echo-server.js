00bf7dd710e0d6280c1daafa2c56cf8e47c43bd4
Eric Herman <eric@freesa.org>
2020-04-11 14:50:42 +0200

    add debug logging, expose logger
    co-authored-by: Ace Medlock <ace.medlock@gmail.com>
    co-authored-by: Kendrick Shaw <kms15@case.edu>
    co-authored-by: Rutger Bazen <rutger.bazen@gmail.com>

--- fake-sample-server/src/echo-server.js
+++ fake-sample-server/src/echo-server.js
@@ -5,11 +5,24 @@ var fs = require('fs'),
     WebSocket = require('ws');
 
 const echoServerConnectMessage = 'Hello world';
+const DEBUG = ((process.env.DEBUG !== undefined) && (process.env.DEBUG !== "0"));
+
+var logger = console;
+
+function log_debug(msg) {
+   if (DEBUG) {
+	logger.log(msg);
+   }
+}
+
+log_debug(`DEBUG: ${DEBUG}`);
 
 function createEchoServer(hostname, port) {
 
     const server = http.createServer(function(req, res) {
-        fs.readFile(__dirname + "/static/" + req.url, function(err, data) {
+	var filename = __dirname + "/static/" + req.url;
+	log_debug(`serving ${filename}`);
+        fs.readFile(filename, function(err, data) {
             if (err) {
                 res.writeHead(404);
                 res.end(JSON.stringify(err));
@@ -32,27 +45,33 @@ function createEchoServer(hostname, port) {
         const pathname = request.url;
         if (pathname === '/sub') {
             wss1.handleUpgrade(request, socket, head, function done(ws) {
+		log_debug(`adding receiver`);
                 receivers.add(ws);
                 ws.on('close', function clear() {
+		    log_debug(`removing receiver`);
                     receivers.delete(ws);
                 });
+		log_debug(`sending echoServerConnectMessage`);
                 ws.send(echoServerConnectMessage);
             });
         } else if (pathname == '/pub') {
             wss1.handleUpgrade(request, socket, head, function done(ws) {
                 ws.on('message', function incoming(data) {
+		    log_debug(`sending to ${receivers.size} receivers`);
+		    log_debug(`   data: ${data}`); // log_trace?
                     receivers.forEach((rws) => {
                         rws.send(data);
                     });
                 });
             });
         } else {
+            log_debug(`calling socket.destroy`);
             socket.destroy();
         }
     });
 
     server.listen(port, hostname, () => {
-        console.log(`Node.js server is running on http://${hostname}:${port}/`);
+        logger.log(`Node.js server is running on http://${hostname}:${port}/`);
     });
 
     return server;
@@ -60,3 +79,4 @@ function createEchoServer(hostname, port) {
 
 exports.echoServerConnectMessage = echoServerConnectMessage;
 exports.createEchoServer = createEchoServer;
+exports.echoServerLogger = logger;
