0d9224148e23ff491e69e1fcb7aeeff49ed1bb57
Ace Medlock <ace.medlock@gmail.com>
2020-10-24 15:13:44 +0000

    allow for more than 1000 sampels per second
    if more than 1000 sps, then then ever ms send a
    batch in a tight loop
    
    Co-authored-by: Eric Herman <eric@freesa.org>
    Co-authored-by: Kendrick Shaw <kms15@case.edu>

--- fake-sample-server/src/sine-wave-generator.js
+++ fake-sample-server/src/sine-wave-generator.js
@@ -17,8 +17,9 @@ if (process.argv.length > 2 && Number(process.argv[2])) {
     samples_per_second = process.argv[2];
 }
 if (samples_per_second <= 0) {
-    samples_per_second = 60
+    samples_per_second = 60;
 }
+
 console.log(`samples_per_second: ${samples_per_second}`);
 
 const seconds_per_cycle = 4;
@@ -27,21 +28,27 @@ console.log(`cycle every ${seconds_per_cycle} seconds`);
 const ticks_per_cycle = seconds_per_cycle * samples_per_second;
 console.log(`data points per cycle: ${ticks_per_cycle}`);
 
-const interval_ms = 1000 / samples_per_second;
+const interval_ms = Math.max(1, 1000 / samples_per_second);
 console.log(`timer interval ms: ${interval_ms}`);
+
+var last_update_time_ms = new Date().getTime();
+const sample_interval_ms = 1000 / samples_per_second;
 
 console.log("begining ...");
 setInterval(() => {
-    loop_counter++;
-    ticks_counter++;
-    const time_ms = new Date().getTime();
-    const radians = ticks_counter * 2 * Math.PI / ticks_per_cycle;
-    const signal_val = Math.sin(radians);
-    const message = `[${loop_counter},${time_ms},${signal_val}]`;
-    try {
-        wspub.send(message);
-    } catch (err) {
-        console.log(err.message);
+    const new_update_time = new Date().getTime();
+    while (last_update_time_ms + sample_interval_ms <= new_update_time) {
+        loop_counter++;
+        ticks_counter++;
+        last_update_time_ms += sample_interval_ms;
+        const radians = ticks_counter * 2 * Math.PI / ticks_per_cycle;
+        const signal_val = Math.sin(radians);
+        const msg = `[${loop_counter},${last_update_time_ms},${signal_val}]`;
+        try {
+            wspub.send(msg);
+        } catch (err) {
+            console.log(err.message);
+        }
     }
     if (ticks_counter > ticks_per_cycle) {
         console.log(`completed a cycle, loop_counter: ${loop_counter}`);
